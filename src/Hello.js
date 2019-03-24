db1c85527b269b7673349b3feda43533a3ccfb3f
Sébastien CEBULA <sebastien.cebula@gmail.com>
2019-03-24 13:10:33 +0100

    Introduction to react

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="f1 tc">
+      <h1>Hello</h1>
+      <p>Welcome to React !</p>
+      </div>
+    )
+  }
+}
+export default Hello;
