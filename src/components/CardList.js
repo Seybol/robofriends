2dd06ccfd26d5340b83ea82e6c2c59c7bdceae21
Sébastien CEBULA <sebastien.cebula@gmail.com>
2020-01-31 17:54:48 +0100

    deleted throwing error

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,6 @@ import React, {Fragment} from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error('Nooooooo');
-  }
   return (
     <Fragment>
       {
